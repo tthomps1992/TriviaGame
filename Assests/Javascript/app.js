e00e5660ccb330ea463c99d7156a75d632b178c5
JC <thompsonrell@gmail.com>
2018-12-20 18:49:56 -0500

    changes

---
+++ Assests/Javascript/app.js
@@ -0,0 +1,13 @@
+function submitAnswers() {
+    let total = 5;
+    let score = 0;
+
+    //User input 
+    let q1 = document.forms["triviaForm"]["q1"].value;
+    let q2 = document.forms["triviaForm"]["q2"].value;
+    let q3 = document.forms["triviaForm"]["q3"].value;
+    let q4 = document.forms["triviaForm"]["q4"].value;
+    let q5 = document.forms["triviaForm"]["q5"].value; 
+
+    alert(q1);
+}
