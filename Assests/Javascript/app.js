55324e4d1c1c8dc59029ae39de9c147f596f9c33
JC <thompsonrell@gmail.com>
2019-01-01 10:03:26 -0500

    timer update

--- Assests/Javascript/app.js
+++ Assests/Javascript/app.js
@@ -1,4 +1,27 @@
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval (function() {
+        minutes = parseInt(timer / 60,10)
+        seconds = parseInt (timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes: minutes;
+        seconds = seconds < 10 ? "0" + seconds: seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+
+        }
+    }, 1000)
+
+}
+
+window.onload = function () {
+    let twoMinutes = 60*2;
+    display = document.querySelector('#timer');
+    startTimer(twoMinutes, display);
+};
 
 function submitAnswers() {
     let total = 5;
@@ -42,6 +65,8 @@ return false;
 
 
 }
+
+
 
 
 
